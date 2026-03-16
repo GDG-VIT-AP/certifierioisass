012469af647dcb732957713ac557f4e8deb80cad
udhaya <udhayanidhi10f@gmail.com>
2026-03-16 22:10:04 +0530

    feat: added API routes and DB scheme

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,15 +1,7 @@
-// defining a simple type for our mock database records
-export type CertificateRecord = {
-    cert_id: string;
-    participant_name: string;
-    image: string;
-    image_size: { width: number; height: number };
-    event_name: string;
-    type: string;
-    name_layer: any;
-    cert_id_layer: any;
-    cert_qr_layer: any;
-};
+import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-// This acts as our temporary database
-export const certificatesDB: CertificateRecord[] = [];
+// Pass the accelerateUrl directly into the constructor
+export const prisma = new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL
+}).$extends(withAccelerate());
